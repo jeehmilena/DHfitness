744d0158a40db24b0d049a1e02dcb4bd268c69b1
jeehmilena <jessicamilenacorrea@gmail.com>
2019-09-25 18:21:01 -0300

    projeto DHfitness

---
+++ app/src/main/java/com/example/dhfitness/model/Usuario.java
@@ -0,0 +1,78 @@
+package com.example.dhfitness.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Usuario implements Parcelable {
+    private String nome;
+    private int idade;
+    private float altura;
+    private float peso;
+
+    public Usuario(String nome, int idade, float altura, float peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Usuario() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    protected Usuario(Parcel in) {
+    }
+
+    public static final Creator<Usuario> CREATOR = new Creator<Usuario>() {
+        @Override
+        public Usuario createFromParcel(Parcel in) {
+            return new Usuario(in);
+        }
+
+        @Override
+        public Usuario[] newArray(int size) {
+            return new Usuario[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+}
---
+++ app/src/main/java/com/example/dhfitness/views/activitys/ResultadosActivity.java
@@ -0,0 +1,19 @@
+package com.example.dhfitness.views.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dhfitness.R;
+
+public class ResultadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+
+        //TODO terminar de implentar a logica para finalizar o projeto
+
+    }
+}
